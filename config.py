a2a6e61f67b5f2a697c6c61a8825d2f72a28bd92
Rgisop2 <160823869+Rgisop2@users.noreply.github.com>
2025-11-30 14:50:21 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,53 +3,44 @@ from logging.handlers import RotatingFileHandler
 
 # Bot Configuration
 LOG_FILE_NAME = "bot.log"
-PORT = '4410'
-OWNER_ID = 1327021082
+PORT = '4410'                         # ← OLD VALUE
+OWNER_ID = 1327021082                 # ← OLD VALUE
 
-MSG_EFFECT = 5046509860389126442
+MSG_EFFECT = 5046509860389126442      # ← OLD VALUE
 
-SHORT_URL = "arolinks.com" # shortner url 
-SHORT_API = "2b3dd0b54ab06c6c8e6cf617f20d5fff15ee1b71" 
+# NEW variable names – OLD values inserted
+SHORT_URL_1 = "arolinks.com"          # OLD SHORT_URL
+SHORT_API_1 = "2b3dd0b54ab06c6c8e6cf617f20d5fff15ee1b71"
+
+SHORT_URL_2 = "arolinks.com"          # same as old (you had only 1)
+SHORT_API_2 = "2b3dd0b54ab06c6c8e6cf617f20d5fff15ee1b71"
+
 SHORT_TUT = "https://t.me/How_to_Download_7x/26"
 
-# Bot Configuration
+# Bot Login
 SESSION = "yato"
 TOKEN = "5717147729:AAHf-p-YAP5Oyor4xKToTZKlr9TC6Wt1JOY"
 API_ID = "27353035"
 API_HASH = "cf2a75861140ceb746c7796e07cbde9e"
 WORKERS = 5
 
+# Database
 DB_URI = "mongodb+srv://poulomig644_db_user:d9MMUd5PsTP5MDFf@cluster0.q5evcku.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 DB_NAME = "yato"
 
-FSUBS = [[-1001918476761, True, 10]] # Force Subscription Channels [channel_id, request_enabled, timer_in_minutes]
-# Database Channel (Primary)
-DB_CHANNEL = -1001918476761   # just put channel id dont add ""
-# Multiple Database Channels (can be set via bot settings)
-# DB_CHANNELS = {
-#     "-1002595092736": {"name": "Primary DB", "is_primary": True, "is_active": True},
-#     "-1001234567890": {"name": "Secondary DB", "is_primary": False, "is_active": True}
-# }
-# Auto Delete Timer (seconds)
-AUTO_DEL = 300
-# Admin IDs
-ADMINS = [1327021082, 1327021082]
-# Bot Settings
+FSUBS = [[-1001918476761, True, 10]]      # OLD VALUE
+
+DB_CHANNEL = -1001918476761               # OLD VALUE
+
+AUTO_DEL = 300                            # OLD VALUE
+
+ADMINS = [1327021082, 1327021082]         # OLD VALUE
+
 DISABLE_BTN = True
 PROTECT = True
 
-# Messages Configuration
-MESSAGES = {
-    "START": "<b>›› ʜᴇʏ!!, {first} ~ <blockquote>ʟᴏᴠᴇ ᴘᴏʀɴʜᴡᴀ? ɪ ᴀᴍ ᴍᴀᴅᴇ ᴛᴏ ʜᴇʟᴘ ʏᴏᴜ ᴛᴏ ғɪɴᴅ ᴡʜᴀᴛ ʏᴏᴜ aʀᴇ ʟᴏᴏᴋɪɴɢ ꜰᴏʀ.</blockquote></b>",
-    "FSUB": "<b><blockquote>›› ʜᴇʏ ×</blockquote>\n  ʏᴏᴜʀ ғɪʟᴇ ɪs ʀᴇᴀᴅʏ ‼️ ʟᴏᴏᴋs ʟɪᴋᴇ ʏᴏᴜ ʜᴀᴠᴇɴ'ᴛ sᴜʙsᴄʀɪʙᴇᴅ ᴛᴏ ᴏᴜʀ ᴄʜᴀɴɴᴇʟs ʏᴇᴛ, sᴜʙsᴄʀɪʙᴇ ɴᴏᴡ ᴛᴏ ɢᴇᴛ ʏᴏᴜʀ ғɪʟᴇs</b>",
-    "ABOUT": "<b>›› ғᴏʀ ᴍᴏʀᴇ: @Nova_Flix \n <blockquote expandable>›› ᴜᴘᴅᴀᴛᴇs ᴄʜᴀɴɴᴇʟ: <a href='https://t.me/codeflix_bots'>Cʟɪᴄᴋ ʜᴇʀᴇ</a> \n›› ᴏᴡɴᴇʀ: @ProYato\n›› ʟᴀɴɢᴜᴀɢᴇ: <a href='https://docs.python.org/3/'>Pʏᴛʜᴏɴ 3</a> \n›› ʟɪʙʀᴀʀʏ: <a href='https://docs.pyrogram.org/'>Pʏʀᴏɢʀᴀᴍ ᴠ2</a> \n›› ᴅᴀᴛᴀʙᴀsᴇ: <a href='https://www.mongodb.com/docs/'>Mᴏɴɢᴏ ᴅʙ</a> \n›› ᴅᴇᴠᴇʟᴏᴘᴇʀ: @cosmic_freak</b></blockquote>",
-    "REPLY": "<b>For More Join - @Hanime_Arena</b>",
-    "SHORT_MSG": "<b>📊 ʜᴇʏ {first}, \n\n‼️ ɢᴇᴛ ᴀʟʟ ꜰɪʟᴇꜱ ɪɴ ᴀ ꜱɪɴɢʟᴇ ʟɪɴᴋ ‼️\n\n ⌯ ʏᴏᴜʀ ʟɪɴᴋ ɪꜱ ʀᴇᴀᴅʏ, ᴋɪɴᴅʟʏ ᴄʟɪᴄᴋ ᴏɴ ᴏᴘᴇɴ ʟɪɴᴋ ʙᴜᴛᴛᴏɴ..</b>",
-    "START_PHOTO": "https://graph.org/file/510affa3d4b6c911c12e3.jpg",
-    "FSUB_PHOTO": "https://telegra.ph/file/7a16ef7abae23bd238c82-b8fbdcb05422d71974.jpg",
-    "SHORT_PIC": "https://telegra.ph/file/7a16ef7abae23bd238c82-b8fbdcb05422d71974.jpg",
-    "SHORT": "https://telegra.ph/file/8aaf4df8c138c6685dcee-05d3b183d4978ec347.jpg"
-}
+# Messages
+MESSAGES = { ... (same as old config) ... }
 
 def LOGGER(name: str, client_name: str) -> logging.Logger:
     logger = logging.getLogger(name)
@@ -64,5 +55,4 @@ def LOGGER(name: str, client_name: str) -> logging.Logger:
     logger.setLevel(logging.INFO)
     logger.addHandler(file_handler)
     logger.addHandler(stream_handler)
-
     return logger
