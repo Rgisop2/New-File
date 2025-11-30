e22a16e912c045a6861c964281e3b206a9c7acb7
Rgisop2 <160823869+Rgisop2@users.noreply.github.com>
2025-11-30 15:04:17 +0530

    Update config.py

--- config.py
+++ config.py
@@ -10,8 +10,8 @@ MSG_EFFECT = 5046509860389126442
 
 SHORT_URL = "arolinks.com"  # First verification shortener
 SHORT_API = "2b3dd0b54ab06c6c8e6cf617f20d5fff15ee1b71"
-SHORT_URL_2 = "arolinks.com"  # Second verification shortener (can be different)
-SHORT_API_2 = "2b3dd0b54ab06c6c8e6cf617f20d5fff15ee1b71"
+SHORT_URL_1 = "arolinks.com"  # Second verification shortener (can be different)
+SHORT_API_1 = "2b3dd0b54ab06c6c8e6cf617f20d5fff15ee1b71"
 SHORT_TUT = "https://t.me/How_to_Download_7x/26"
 
 # Bot Configuration
